e7b7926c1edbbe5fe90cfdff9d7513d4daad1c85
lalatro1999 <lalatro1999@gmail.com>
2024-08-02 23:24:47 +0000

    Layouting the mainhome

---
+++ framework/src/home/page.jsx
@@ -0,0 +1,14 @@
+'use client'
+import styles from "./home.module.css"
+
+
+const MainHome = () => {
+  return (
+    <div className="home-container">
+      <div>Columna contactos</div>
+      <div>Columna de mensajes</div>
+    </div>
+  )
+}
+
+export default MainHome
