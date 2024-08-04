72cb712676ecbb27b5f52c7d291ad06046c54688
anthony00052023 <anthony00052023@gmail.com>
2024-08-04 01:18:38 +0000

    create messageCloud y topBar

---
+++ framework/src/app/components/messageCloud/messageCloud.jsx
@@ -0,0 +1,14 @@
+import styles from './messageCloud.module.css'
+
+const Message_Cloud = () => {
+    return(
+        <div className={styles.speech_bubble}>       
+            <p>El mondongo puriscaleño es el mejor.</p>
+        </div>
+        
+        )
+}
+
+
+
+export default Message_Cloud
