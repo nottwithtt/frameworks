839563b66a0c94f0e027775df4580f69fd7b1d78
Macotosama <jtn1999@outlook.com>
2024-08-07 08:54:37 -0600

    Some cookies
    cookies

--- backend/sockets/chatSoket.js
+++ backend/sockets/chatSoket.js
@@ -1,4 +1,10 @@
 const { Server } = require("socket.io");
+const JWT = require("jsonwebtoken");
+const User = require("../models/User");
+const {
+  hashPassword,
+  comparePassword,
+} = require("../utils/authenticationUtils");
 
 module.exports = function (serverPort) {
   const io = new Server(serverPort, {
@@ -11,15 +17,41 @@ module.exports = function (serverPort) {
   });
 
   io.on("connection", (socket) => {
-    console.log("a user connected");
-    
-    socket.on("message", (data) => {
-      console.log(data);
-      io.emit("message", data);
+    socket.on("addNewUser", (userId) => {
+      // const decoded = JWT.verify(token, process.env.ACCESS_TOKEN_SEQUENCE);
+      // const user = await User.findById(decoded.id);
+      !onlineUsers.some((user) => user.userId === userId) &&
+        onlineUsers.push({
+          userId,
+          socketId: socket.id,
+        });
+      
+    socket.on("sendMessage", (message) => {
+      const user = onlineUsers.find(
+        (user) => user.userId === message.recipientId
+      );
+      
+      if (user) {
+        console.log("sending message");
+        io.to(user.socketId).emit("getMessage", message);
+      }
     });
-
-    socket.on("disconnect", () => {
-      console.log("user disconnected");
+  
+    console.log("Connected Users:", onlineUsers);
     });
   });
+
+//  io.on("connection", (socket) => {
+    // console.log("a user connected");
+    
+  //   socket.on("message", (data) => {
+  //     console.log(data);
+  //     io.emit("message", data);
+  //   });
+
+  //   socket.on("disconnect", () => {
+  //     console.log("user disconnected");
+  //   });
+  // });
+
 };
